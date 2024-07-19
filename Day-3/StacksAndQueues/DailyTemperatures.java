b59c0276d9dc88e7e15cec4b92c26fce6aede4ed
swanith1234 <swanithpidugu@gmail.com>
2024-07-19 21:24:14 +0530

    Day-3 completed

---
+++ Day-3/StacksAndQueues/DailyTemperatures.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int[] dailyTemperatures(int[] temperatures) {
+      int n = temperatures.length;
+      int[] answer = new int[n];
+      Stack<Integer> stack = new Stack<>();
+
+      for (int i = 0; i < n; i++) {
+          while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+              int index = stack.pop();
+              answer[index] = i - index;
+          }
+          stack.push(i);
+      }
+
+      // The remaining indices in the stack have no warmer days ahead
+      while (!stack.isEmpty()) {
+          answer[stack.pop()] = 0;
+      }
+
+      return answer;
+  }
+}
